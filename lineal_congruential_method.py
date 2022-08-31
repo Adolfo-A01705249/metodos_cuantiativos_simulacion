a8555f69a552e8b36c4aead032b0192a49fa3841
GlinkIsCoding <carlos.espino.m@gmail.com>
2022-08-31 15:59:26 -0500

    User input and file output added

--- lineal_congruential_method.py
+++ lineal_congruential_method.py
@@ -7,15 +7,22 @@
 # Date: 30/august/2022
 # Class: Metodos cuantitativos y simulacion
 
-Xo = 6
-a = 32
-c = 3
-m = 80
+# Inputs
+Xo = int(input("Enter the value of Xo: ")) #6
+a = int(input("Enter the value of a: ")) #32
+c = int(input("Enter the value of c: ")) #3
+m = int(input("Enter the value of m: ")) #80
+
 Xi = Xo
 
 res = 0
 
+lcfile = open('lcmethod.txt', 'w')
+print("Linear Congruential Method\nThe numbers are:\n", file = lcfile)
 for x in range(10):
     Xi = ((a * Xi) + c) % m
     res = float(Xi / m)
     print(res)
+    print(res, file = lcfile)
+    
+lcfile.close()
